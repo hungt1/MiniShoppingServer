dd72cb5a37ab51dcb98d95536c1c89985484bb00
vanloc1808 <vanloc1808@gmail.com>
2023-01-07 17:16:01 +0700

    edit

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-source newenv/bin/activate
-python manage.py runserver 8000
+python3 -m venv ./venv
+source venv/bin/activate
+pip install -r requirements.txt
+pip install scikit-learn
+pip install sklearn
+pip install pretty-html-table
+chmod a+x run.sh
+python3 manage.py runserver 8000
