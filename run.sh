586bb894d7e1226b3892e231464f7c1bf9300479
vanloc1808 <vanloc1808@gmail.com>
2023-01-08 11:43:40 +0700

    final

--- run.sh
+++ run.sh
@@ -3,6 +3,4 @@ source venv/bin/activate
 pip install -r requirements.txt
 pip install scikit-learn
 pip install sklearn
-pip install pretty-html-table
-chmod a+x run.sh
 python3 manage.py runserver 8000
